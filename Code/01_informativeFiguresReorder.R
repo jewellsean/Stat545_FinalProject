a30726be743ace68aa56d2a7830ee7463315563b
Sean Jewell <jewellsean@gmail.com>
2013-10-19 11:29:39 -0700

    changed file saving syntax for easier pattern recon. added code to order the factors write file to source and added details for log file

--- Code/01_informativeFiguresReorder.R
+++ Code/01_informativeFiguresReorder.R
@@ -14,14 +14,22 @@ q <- q + geom_point(aes(size = sqrt(pop/pi), fill = continent), pch = 21, show_g
             scale_size_continuous(range = c(1,40)) + facet_wrap(~continent) + 
             ggtitle(paste("Life Expectancy vs. GDP per Capita in ", jYear, sep = ""))
 
-ggsave(filename = paste("lifeExpectancyVsGdpPerCap",jYear,".pdf", sep = ""), plot = q, path = "Figures")
+ggsave(filename = paste("inform_lifeExpectancyVsGdpPerCap_",jYear,".pdf", sep = ""), plot = q, path = "Figures")
 
 # Second informative plot:
 
-# Reorder the continents based on life expectancy: 
-# 
+# look at some of the plots we have done in the past. 
+
+# Reorder the continents based on life expectancy, and clean data to remove Oceania 
+cleanData <- droplevels(subset(gDat, continent != "Oceania"))
+table(cleanData$continent)  #ensure that the factor has been dropped
+
+cleanData <- within(cleanData, continent <- reorder(continent, lifeExp))
+table(cleanData$continent)  #ensure that the factor has been reordered on lifeExp
 
 
+Write the sorted data to file for future use
+write.table(cleanData, "Data/gapminder_clean.tsv", quote = FALSE, sep = "\t", row.names = FALSE) 
 
-# Write the sorted data to file for future use
-# write.table(jCoefs, "Data/jCoefs.txt", quote = FALSE, sep = "\t", row.names = FALSE) ## place holder for now
+sessionInfo()
+Sys.time()
