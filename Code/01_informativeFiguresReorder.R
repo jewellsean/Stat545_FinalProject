d48449554cf1c285565760c389b9995bd751c530
Sean Jewell <jewellsean@gmail.com>
2013-10-19 23:57:19 -0700

    added informative figures

--- Code/01_informativeFiguresReorder.R
+++ Code/01_informativeFiguresReorder.R
@@ -5,21 +5,33 @@ gDat <- read.delim("Data/gapminderDataFiveYear.txt")
 # Check that data import went successfully
 tail(gDat)
 
-# First informative plot: life expectancy vs. GDP per capita for a particular year
-jYear <- 2002
+# Informative plot: life expectancy vs. GDP per capita for a particular year
+q1 <- ggplot(gDat, aes(x = gdpPercap, y = lifeExp)) + geom_point() +
+  scale_x_log10() + facet_wrap(~ continent) + geom_smooth(method = loess) + 
+  ggtitle("Life Expectancy vs. GDP per Capita")
 
-q <- ggplot(subset(gDat, year == jYear),
+ggsave(filename = "inform_lifeExpectancyVsGdpPerCap_all_loessSmooth.pdf", plot = q1, path = "Figures", width = 14.4, height = 10.9)
+
+# Another informative plot
+sCountry <- c("United States", "Canada", "Mexico") # North America
+
+q2 <- ggplot(data = within(subset(gDat, country %in% sCountry), country <- reorder(country, lifeExp))
+             , aes(x = country, y = lifeExp)) + geom_boxplot() + geom_jitter() +
+  ggtitle("Life Expectancy measures of spread for select countries")
+ggsave(filename = "inform_lifeExpectancy_measuresOfSpread.pdf", plot = q1, path = "Figures", width = 14.4, height = 10.9)
+
+
+# Informative plot: look at a particular year per continent
+sYear <- 2002
+
+q3 <- ggplot(subset(gDat, year == sYear),
             aes(x = gdpPercap, y = lifeExp)) + scale_x_log10()
 
-q <- q + geom_point(aes(size = sqrt(pop/pi), fill = continent), pch = 21, show_guide = FALSE) +
+q3 <- q3 + geom_point(aes(size = sqrt(pop/pi), fill = continent), pch = 21, show_guide = FALSE) +
             scale_size_continuous(range = c(1,40)) + facet_wrap(~continent) + 
-            ggtitle(paste("Life Expectancy vs. GDP per Capita in ", jYear, sep = ""))
+            ggtitle(paste("Life Expectancy vs. GDP per Capita in ", sYear, sep = ""))
 
-ggsave(filename = paste("inform_lifeExpectancyVsGdpPerCap_",jYear,".pdf", sep = ""), plot = q, path = "Figures", width = 14.4, height = 10.9)
-
-# Second informative plot:
-
-# look at some of the plots we have done in the past. 
+ggsave(filename = paste("inform_lifeExpectancyVsGdpPerCap_",sYear,".pdf", sep = ""), plot = q3, path = "Figures", width = 14.4, height = 10.9)
 
 # Reorder the continents based on life expectancy, and clean data to remove Oceania 
 cleanData <- droplevels(subset(gDat, continent != "Oceania"))
